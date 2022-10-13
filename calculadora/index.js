953742f3214893141cfe4f89dff1e7cceab21476
= <=>
2022-10-13 14:42:52 -0300

    finalização

--- calculadora/index.js
+++ calculadora/index.js
@@ -12,9 +12,10 @@ for (let botao of btnNumeros) {
 }
 
 function cliqueNumero(event) {
-    if (isNaN(visor.innerHTML) === false)
+    if (isNaN(visor.innerHTML+event.target.innerHTML) === false)
         visor.innerHTML = visor.innerHTML + event.target.innerHTML;
-    else visor.innerHTML = event.target.innerHTML;
+    else 
+        visor.innerHTML = event.target.innerHTML;
 }
 for (let botao of btnOperador) {
     botao.addEventListener("click", cliqueOperdaor);
@@ -64,10 +65,10 @@ function calculaResultado(event) {
     }
 }
 function executaOperacao(valor1, operador, valor2) {
-    if (operador === "×") {
-        return valor1 * valor2;
-    } else if (operador === "÷") {
-        return valor1 / valor2;
+    if (operador === "*") {
+        return (valor1 * valor2).toFixed(4);
+    } else if (operador === "/") {
+        return (valor1 / valor2).toFixed(4);
     } else if (operador === "-") {
         return valor1 - valor2;
     } else if (operador === "+") {
@@ -77,13 +78,45 @@ function executaOperacao(valor1, operador, valor2) {
 let body = document.getElementsByTagName("body")[0];
 body.addEventListener("keydown", pressionouTecla);
 function pressionouTecla(event) {
-    if (event.key === "×") {
+    if (event.key === "*") {
         document.getElementById("botaoMultiplicacao").click();
-    } else if (event.key === "÷") {
+    } else if (event.key === "/") {
         document.getElementById("botaoDivisao").click();
     } else if (event.key === "-") {
         document.getElementById("botaoMenos").click();
     } else if (event.key === "+") {
         document.getElementById("botaoMais").click();
+    }else if(event.key === "Escape"){
+        document.getElementById("botaoAC").click();
+    }else if (event.key === "1"){
+        document.getElementById("botao-um").click();
+    }else if (event.key === "2"){
+        document.getElementById("botao-dois").click();
+    }else if (event.key === "3"){
+        document.getElementById("botao-tres").click();
+    }else if (event.key === "4"){
+        document.getElementById("botao-quatro").click();
+    }else if (event.key === "5"){
+        document.getElementById("botao-cinco").click();
+    }else if (event.key === "6"){
+        document.getElementById("botao-seis").click();
+    }else if (event.key === "7"){
+        document.getElementById("botao-sete").click();
+    }else if (event.key === "8"){
+        document.getElementById("botao-oito").click();
+    }else if (event.key === "9"){
+        document.getElementById("botao-nove").click();
+    }else if (event.key === "0"){
+        document.getElementById("botao-zero").click();
+    }else if (event.key === "E"){
+        document.getElementById("botao-um").click();
+    }else if (event.key === "Backspace"){
+        document.getElementById("botaoApagaUm").click();
+    }else if (event.key === "Enter"){
+        document.getElementById("botaoIgual").click();
+    }else if (event.key === "."){
+        document.getElementById("botao-ponto").click();
+    }else if (event.key === ","){
+    document.getElementById("botao-ponto").click();
     }
 }
